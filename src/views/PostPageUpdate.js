54dbf557ee4bbc3e3caf4356e7eca5e4cb1cd833
kangheong <kangheong@gmail.com>
2023-03-30 10:15:00 +0800

    first commit

---
+++ src/views/PostPageUpdate.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate, useParams } from "react-router-dom";
+import { auth, db } from "../firebase";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { signOut } from "firebase/auth";
+
+export default function PostPageUpdate() {
+  const params = useParams();
+  const id = params.id;
+  const [caption, setCaption] = useState("");
+  const [image, setImage] = useState("");
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  async function updatePost() {
+    await updateDoc(doc(db, "posts", id), { caption, image });
+    navigate("/");
+  }
+
+  async function getPost(id) {
+    const postDocument = await getDoc(doc(db, "posts", id));
+    const post = postDocument.data();
+    setCaption(post.caption);
+    setImage(post.image);
+  }
+
+  useEffect(() => {
+    if (loading) return;
+    if (!user) navigate("/login");
+    getPost(id);
+  }, [id, loading, navigate, user]);
+
+  return (
+    <div>
+      <Navbar variant="light" bg="light">
+        <Container>
+          <Navbar.Brand href="/">Tinkergram</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/add">New Post</Nav.Link>
+            <Nav.Link onClick={ (e) => signOut(auth)}>🚪</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container>
+        <h1 style={{ marginBlock: "1rem" }}>Update Post</h1>
+        <Form>
+          <Form.Group className="mb-3" controlId="caption">
+            <Form.Label>Caption</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Lovely day"
+              value={caption}
+              onChange={(text) => setCaption(text.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="image">
+            <Form.Label>Image URL</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="https://zca.sg/img/1"
+              value={image}
+              onChange={(text) => setImage(text.target.value)}
+            />
+            <Form.Text className="text-muted">
+              Make sure the url has a image type at the end: jpg, jpeg, png.
+            </Form.Text>
+          </Form.Group>
+          <Button variant="primary" onClick={(e) => updatePost()}>
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
+}
